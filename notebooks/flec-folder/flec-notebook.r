182081b286018e47de41792718a271e87ac35b23
Fleur Lecarp <fleur.lecarpentier@gmail.com>
2019-06-25 15:54:29 +0200

    baby R example with titanic

--- notebooks/flec-folder/flec-notebook.r
+++ notebooks/flec-folder/flec-notebook.r
@@ -3,8 +3,38 @@ library(dplyr)
 
 # COMMAND ----------
 
-library(shiny)
+install.packages("rpart")
+install.packages("rpart.plot")
 
 # COMMAND ----------
 
-install.packages("shiny")
+install.packages("caret")
+install.packages("e1071")
+
+# COMMAND ----------
+
+library(rpart.plot)
+library(caret)
+
+# COMMAND ----------
+
+data(ptitanic)
+ptitanic <- na.omit(ptitanic)
+
+# COMMAND ----------
+
+ind <- createDataPartition(ptitanic$survived, 1, p=0.8, list=T)
+trainDB <- ptitanic[ind$Resample1,]
+
+# COMMAND ----------
+
+glm_titanic <- caret::train(survived ~sex + age +pclass, data=trainDB, method = "glm")
+
+# COMMAND ----------
+
+glm_titanic$finalModel$coefficients
+
+# COMMAND ----------
+
+(test <- trainDB[1,c("pclass", "sex", "age")])
+predict(glm_titanic, newdata = test)
